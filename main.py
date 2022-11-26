606d6480d5639bc338ae0f83ece7260b6541d24a
Yurnero <82593889+jeanmav@users.noreply.github.com>
2022-11-26 21:12:01 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import kivy
+from kivymd.app import MDApp
+from kivy.core.window import Window
+
+Window.size = (480, 800)
+
+
+class BSCSLoginPage(MDApp):
+    def build(self):
+        return
+
+
+class BSCSMainPage(MDApp):
+    def build(self):
+        return
+
+
+BSCSMainPage().run()

b60f4078c1880ca3e1d8a2169bdcfca24f807e30
Jhon Lloyd F. Omblero <113282952+joxyle-jhon04@users.noreply.github.com>
2022-11-26 23:28:38 +0800

    makabuang data base

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import kivy
+from kivy.properties import ListProperty
+from kivy.core.text import LabelBase
 from kivymd.app import MDApp
+from kivy.lang import Builder
 from kivy.core.window import Window
+import mysql.connector
 
 Window.size = (480, 800)
 
 
 class BSCSLoginPage(MDApp):
-    def build(self):
-        return
+
+    database = mysql.connector.Connect(host="localhost", user="root", password="2004130304", database="loginform")
+    cursor = database.cursor()
+    cursor.execute("select * from logindata")
+    for i in cursor.fetchall():
+        print(i[0], i[1]) 
+
+def send_data(self, email, password):
+#here is the function to send data from python to mysql
+        self.cursor.execute(f"insert into logindata values('{email.text}', '{password.text}')")
+        self.database.commit()
+        for i in self.cursor.fetchall():
+            print(i[0], i[1])
+
 
 
 class BSCSMainPage(MDApp):
@@ -15,4 +31,4 @@ class BSCSMainPage(MDApp):
         return
 
 
-BSCSMainPage().run()
+BSCSLoginPage().run()

1f750d235a4688a6c8c9c70691db6f54ae252ac2
Yurneroo <82593889+Yurneroo@users.noreply.github.com>
2022-11-28 13:09:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,25 +10,17 @@ Window.size = (480, 800)
 
 
 class BSCSLoginPage(MDApp):
-
-    database = mysql.connector.Connect(host="localhost", user="root", password="2004130304", database="loginform")
-    cursor = database.cursor()
-    cursor.execute("select * from logindata")
-    for i in cursor.fetchall():
-        print(i[0], i[1]) 
-
-def send_data(self, email, password):
-#here is the function to send data from python to mysql
-        self.cursor.execute(f"insert into logindata values('{email.text}', '{password.text}')")
-        self.database.commit()
-        for i in self.cursor.fetchall():
-            print(i[0], i[1])
-
+    pass
 
 
 class BSCSMainPage(MDApp):
+    pass
+
+
+class BSCSApp(MDApp):
     def build(self):
-        return
+        return Builder.load_file("BSCSMainPage.kv")
 
 
-BSCSLoginPage().run()
+if __name__ == "__main__":
+    BSCSMainPage().run()
